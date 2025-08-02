b8a8766be94249b0a58186858aaf4bf177a9f30f
Christian <alch.infoemail@gmail.com>
2025-08-02 22:04:39 +0200

    fix(routing): docs in the root

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,7 +1,60 @@
-import type { ReactNode } from 'react';
-import { HomeLayout } from 'fumadocs-ui/layouts/home';
-import { baseOptions } from '@/app/layout.config';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import type { ReactNode } from "react";
+import { baseOptions } from "@/app/layout.config";
+import { source } from "@/lib/source";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <HomeLayout {...baseOptions}>{children}</HomeLayout>;
+  return (
+    <DocsLayout
+      tree={{
+        name: "docs",
+        children: [
+          {
+            name: "Introduction",
+            url: "/docs",
+            type: "page",
+          },
+          {
+            name: "Installation",
+            type: "folder",
+            children: [
+              {
+                name: "Node.js",
+                url: "/docs/installation/node",
+                type: "page",
+              },
+              {
+                name: "Python",
+                url: "/docs/installation/python",
+                type: "page",
+              },
+            ],
+          },
+          ...source
+            .getPageTree()
+            .children?.filter((child) => child.$id === "rest"),
+          {
+            name: "Saving",
+            url: "/docs/saving",
+            type: "page",
+          },
+          {
+            name: "Retrieve",
+            url: "/docs/retrieve",
+            type: "page",
+          },
+          {
+            name: "Injection",
+            url: "/docs/injection",
+            type: "page",
+          },
+        ],
+      }}
+      {...baseOptions}
+    >
+      {children}
+    </DocsLayout>
+  );
 }
